08354452295bba44651c871509749142392f5f07
topcook <codecooker1996@gmail.com>
2022-05-12 03:15:40 -0400

    simple chart example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Pie, Doughnut} from 'react-chartjs-2';
+import {Chart, ArcElement} from 'chart.js'
+Chart.register(ArcElement);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const state = {
+  labels: ['January', 'February', 'March',
+           'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      backgroundColor: [
+        '#B21F00',
+        '#C9DE00',
+        '#2FDE00',
+        '#00A6B4',
+        '#6800B4'
+      ],
+      hoverBackgroundColor: [
+      '#501800',
+      '#4B5000',
+      '#175000',
+      '#003350',
+      '#35014F'
+      ],
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
 }
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{width:'300px'}}>
+        <Pie
+          data={state}
+          // options={{
+          //   title:{
+          //     display:true,
+          //     text:'Average Rainfall per month',
+          //     fontSize:20
+          //   },
+          //   legend:{
+          //     display:true,
+          //     position:'right'
+          //   }
+          // }}
+        />
+
+        <Doughnut
+          data={state}
+          // options={{
+          //   title:{
+          //     display:true,
+          //     text:'Average Rainfall per month',
+          //     fontSize:20
+          //   },
+          //   legend:{
+          //     display:true,
+          //     position:'right'
+          //   }
+          // }}
+        />
+      </div>
+    );
+  }
+}
